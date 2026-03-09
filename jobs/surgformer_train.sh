d06d7a8557e873b6705aeeaa801ed5539d2c4b03
Andrei Simionescu <andreisimi02@gmail.com>
2026-03-09 12:00:47 +0100

    auto resume model

--- jobs/surgformer_train.sh
+++ jobs/surgformer_train.sh
@@ -43,7 +43,7 @@ export NCCL_P2P_DISABLE=0
 PYTHONUNBUFFERED=1 torchrun --nproc_per_node=${NUM_GPUS} downstream_phase/run_phase_training.py \
     --batch_size 24 \
     --epochs 50 \
-    --save_ckpt_freq 10 \
+    --save_ckpt_freq 5 \
     --model surgformer_HTA_KCA \
     --pretrained_path "${PRETRAIN_PATH}" \
     --mixup 0.8 \
@@ -65,5 +65,5 @@ PYTHONUNBUFFERED=1 torchrun --nproc_per_node=${NUM_GPUS} downstream_phase/run_ph
     --log_dir "${PROJECT_DIR}/results/Cholec80" \
     --num_workers 4 \
     --clip_grad 1.0 \
-    --no_auto_resume \
+    --auto_resume \
     --cut_black
